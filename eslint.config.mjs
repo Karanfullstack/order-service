187904e082bf36a8ce4dab436bbf895f0ce17ec9
Karan Chauhan <karanchouhan211@gmail.com>
2024-10-16 16:46:04 +0200

    add winston logger

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,25 +3,25 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 export default tseslint.config(
-    {
-        rules: {
-            'dot-notation': 'error',
-            'no-console': 'error',
-        },
-    },
-    {
-        ignores: ['dist/**/*.ts', 'dist/**', '**/*.mjs', 'eslint.config.mjs', '**/*.js'],
-    },
-    eslintConfigPrettier,
-    eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    {
-        languageOptions: {
-            parserOptions: {
-                project: './tsconfig.json',
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-    },
+   {
+      rules: {
+         'dot-notation': 'error',
+         'no-console': 'error',
+      },
+   },
+   {
+      ignores: ['dist/**/*.ts', 'dist/**', '**/*.mjs', 'eslint.config.mjs', '**/*.js'],
+   },
+   eslintConfigPrettier,
+   eslint.configs.recommended,
+   ...tseslint.configs.recommendedTypeChecked,
+   {
+      languageOptions: {
+         parserOptions: {
+            project: './tsconfig.json',
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+         },
+      },
+   },
 );
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import { Config } from './config';
 import app from './app';
+import logger from './config/logger';
 
 const startServer = () => {
-    try {
-        app.listen(Config.PORT, () => {
-            // eslint-disable-next-line no-console
-            console.log(`Server is running at http://localhost:${Config.PORT}`);
-        });
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(error);
-        process.exit(1);
-    }
+   try {
+      app.listen(Config.PORT, () => {
+         logger.info(`Server is running at http://localhost:${Config.PORT}`);
+      });
+   } catch (error) {
+      if (error instanceof Error) {
+         logger.error(error.message);
+         setTimeout(() => process.exit(1), 1000);
+      }
+   }
 };
 
 startServer();
