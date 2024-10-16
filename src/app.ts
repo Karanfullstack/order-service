9abdbe6d9cb915e628d7c64936a3f99e33d6cf51
Karan Chauhan <karanchouhan211@gmail.com>
2024-10-16 17:36:33 +0200

    add error handling config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger';
+import createHttpError, { HttpError } from 'http-errors';
 
 const app = express();
 
-app.get('/', (req, res) => {
-   res.send('Hello World');
+app.get('/', (req, res, next) => {
+   const error = createHttpError(401, 'you are not authorized');
+   return next(error);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
+   logger.error(error.message);
+   const statusCode = error.statusCode || 500;
+
+   res.status(statusCode).json({
+      errors: [
+         {
+            type: error.name,
+            msg: error.message,
+            path: '',
+            location: '',
+         },
+      ],
+   });
 });
 
 export default app;
